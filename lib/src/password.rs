5d66f825498b46814c3d21e74bf460de42b59366
Jayson Reis <santosdosreis@gmail.com>
2021-03-31 14:47:29 +0200

    chore: Add test for our derive_key function
    This ensures that it is using the same parameters as authy

--- lib/src/password.rs
+++ lib/src/password.rs
@@ -16,3 +16,17 @@ pub(crate) fn derive_key(backup_password: &str, salt: &str) -> Vec<u8> {
 
     derived_key.to_vec()
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::password::derive_key;
+
+    #[test]
+    fn test_derive_key_implementation() {
+        let expected = [
+            84, 238, 29, 216, 57, 143, 244, 224, 255, 82, 192, 61, 32, 22, 16, 55, 101, 165, 19,
+            21, 21, 89, 206, 233, 116, 212, 54, 78, 196, 147, 85, 132,
+        ];
+        assert_eq!(derive_key("test", "salty"), &expected);
+    }
+}
